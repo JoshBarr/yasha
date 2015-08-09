c37f4a784881e03da0dc64d15cd377f1dd0f5c5d
Josh Barr <josh@joshbarr.com>
2015-08-09 15:25:08 +1200

    work on deploy script

--- deployment/post_deploy.sh
+++ deployment/post_deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
+cd ~/webapps/yasha/
 # Flush cache, deactivate venv and restart services
 redis-cli FLUSHALL
 service uwsgi restart
 # Among these two, choose your own command wisely
-apache2ctl graceful
-service nginx restart
+# apache2ctl graceful
+apache2/bin/restart
+# service nginx restart
